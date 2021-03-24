b43efb96e0b3a36ebfd85b74a08047f5233b949b
Hayden <haydenrayne.rooney@gmail.com>
2021-03-23 21:27:31 -0400

    Fixed models and linked postgresql

--- poll_fighters/__init__.py
+++ poll_fighters/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://appuser:apppassword@localhost:5432/appdb'
 db = SQLAlchemy(app)
--- poll_fighters/models.py
+++ poll_fighters/models.py
@@ -1,29 +1,31 @@
 from poll_fighters import db
+from sqlalchemy import Column, Integer, ForeignKey, TEXT, Table, String
+from sqlalchemy.orm import declarative_base, relationship
 
-class Base(db.Model):
-    pass
+Base = declarative_base()
 
 class User(Base):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-association_table = db.Table('association', Base.metadata,
+association_table = Table('association', Base.metadata,
     Column('poll_id', Integer, ForeignKey('polls.id')),
     Column('right_id', Integer, ForeignKey('questions.id'))
 )
 
 class Poll(Base):
     __tablename__ = 'polls'
-    id = db.Column(db.Integer, primary_key=True)
-    description = id.Column(db.TEXT)
+    id = Column(Integer, primary_key=True)
+    description = Column(TEXT)
     questions = relationship("Question", secondary=association_table,back_populates="parents")
 
 class Question(Base):
     __tablename__ = 'questions'
-    id = db.Column(db.Integer, primary_key=True)
-    text = id.Column(db.TEXT, unique=True)
+    id = Column(Integer, primary_key=True)
+    text = Column(TEXT, unique=True)
     
