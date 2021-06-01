d9ebc5e2c394533036c7b55502899cb08600ce35
Hayden <haydenrayne.rooney@gmail.com>
2021-06-01 18:25:57 -0400

    Started creating response model

--- poll_fighters/models.py
+++ poll_fighters/models.py
@@ -2,9 +2,6 @@ from poll_fighters import db
 from sqlalchemy import Column, Integer, ForeignKey, TEXT, Table, String
 from sqlalchemy.orm import declarative_base, relationship
 
-# To Do:
-# Create an Id mixin
-
 class IdMixin:
     id = Column(Integer, primary_key=True)
 
@@ -24,13 +21,18 @@ class User(Base, IdMixin):
 
 association_table = Table('association', Base.metadata,
     Column('poll_id', Integer, ForeignKey('polls.id')),
-    Column('right_id', Integer, ForeignKey('questions.id'))
+    Column('question_id', Integer, ForeignKey('questions.id'))
 )
 
 class Poll(Base, IdMixin):
     __tablename__ = 'polls'
     description = Column(TEXT)
     questions = relationship("Question", secondary=association_table)
+
+    def __init__(self, description, questions=None):
+        self.description = description
+        if questions != None:
+            self.questions = questions
 
 
 class Question(Base, IdMixin):
@@ -43,11 +45,54 @@ class Question(Base, IdMixin):
         self.text = text
         self.question_type_id = question_type_id
 
+    def __eq__(self, other):
+        return self.text == other.text and self.question_type_id == other.question_type_id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 class QuestionType(Base, IdMixin):
     __tablename__ = 'question_types'
     name = Column(String(20), unique=True, nullable=False)
     description = Column(TEXT)
-    
+    constant_types = ['Multiple', 'Open', 'Radio']
+
     def __init__(self, name, description=None):
         self.name = name
         self.description = description
+
+class Choice(Base, IdMixin):
+    __tablename__ = 'choices'
+    text = Column(String(35))
+
+# class Responses(Base, IdMixin): 
+#     __tablename__ = 'poll_responses'
+#     poll_id = Column(Integer, ForeignKey('polls.id'))
+#     responder_user_id = Column(Integer, ForeignKey('users.id'))
+#     question_id = Column(Integer, ForeignKey('questions.id'))
+#     open_response = Column(TEXT, nullable=True)
+#     choice_id = Column(Integer, ForeignKey('choices.id'), nullable=True)
+
+class Response(IdMixin):
+    poll_id = Column(Integer, ForeignKey('polls.id'))
+    poll = relationship(Poll)
+    responder_user_id = Column(Integer, ForeignKey('users.id'))
+    responder_user = relationship(User)
+    question_id = Column(Integer, ForeignKey('questions.id'))
+    question = relationship(Question)
+
+class ResponseOpen(Base, Response): 
+    __tablename__ = 'poll_response_open'
+    open_response = Column(TEXT)
+
+class ResponseChoice(Base, Response):
+    __tablename__ = 'poll_response_choice'
+    choice_id = Column(Integer, ForeignKey('choices.id'))
+    choice = relationship(Choice)
+
+# To Do: 
+# Write two new models:
+# 1. the choices for multiple choice
+# 2. the responses from the users 
+# each record in the table of responses would have what poll was it, who responded, what question, what answer
+# could be in a single model or multiple models with relationships
--- test/test_models.py
+++ test/test_models.py
@@ -96,27 +96,121 @@ def test_question_hayden(session):
 
     # Create a question
 
-    new_question = models.Question(text='pizza',question_type=new_type)
+    new_question = models.Question(text='pizza',question_type_id=new_type.id)
+
+def test_add_multiple_question_to_poll(session):
+    # Creates a single type for questions
+    multiple_type = models.QuestionType('Multiple Choice')
+    session.add(multiple_type)
+    session.commit()
+
+    # Creates multiple questions using a single type
+    first_question = models.Question(text='pizza', question_type_id=multiple_type.id)
+    second_question = models.Question(text='sushi', question_type_id=multiple_type.id)
+    third_question = models.Question(text='tacos', question_type_id=multiple_type.id)
+    session.add(first_question)
+    session.add(second_question)
+    session.add(third_question)
+    session.commit()
+    
+    # Creates a poll using the three questions made above
+    food_poll = models.Poll(description="What are your favorite foods?", questions=[first_question,second_question,third_question])
+    session.add(food_poll)
+    session.commit()
+
+    # Selects the poll from the database and asserts that it was assigned all three questions from above
+    select_statement = select(models.Poll).where(models.Poll.id == food_poll.id)
+    response = session.execute(select_statement)
+    queried_poll = response.first()[0]
+    assert queried_poll.questions == [first_question, second_question, third_question]
+
+def test_add_multiple_qestions_of_different_types_in_poll(session):
+    # Creates multiple types for questions
+    multiple_type = models.QuestionType('Multiple Choice')
+    open_type = models.QuestionType('Open Ended')
+    session.add(multiple_type)
+    session.add(open_type)
+    session.commit()
+
+    # Creates Multiple question using the different types above
+    multiple_question = models.Question(text='Minecraft', question_type_id=multiple_type.id)
+    open_question = models.Question(text='What is the meaning of life', question_type_id=open_type.id)
+    session.add(multiple_question)
+    session.add(open_question)
+    session.commit()
+
+    # Creates a poll that includes the different question above
+    random_poll = models.Poll(description='Random questions about life', questions=[multiple_question, open_question])
+    session.add(random_poll)
+    session.commit()
+
+    #
+    select_statement = select(models.Poll).where(models.Poll.id == random_poll.id)
+    response = session.execute(select_statement)
+    queried_poll = response.first()[0]
+    queried_poll.questions.should.contain(open_question)
+    queried_poll.questions.should.contain(multiple_question)
+
+def test_add_multiple_polls(sessions):
+    pass
+
+def find_all_multiple_choice_questions_in_poll(session):
+    multiple_type = models.QuestionType('Multiple Choice')
+    open_type = models.QuestionType('Open Ended')
+    session.add(multiple_type)
+    session.add(open_type)
+    session.commit()
+
+    multiple_question_first = models.Question(text='Minecraft', question_type_id=multiple_type.id)
+    multiple_question_second = models.Question(text='Roblox', question_type_id=multiple_type.id)
+    multiple_question_third = models.Question(text='Apex Legends', question_type_id=multiple_type.id)
+    session.add(multiple_question_first)
+    session.add(multiple_question_second)
+    session.add(multiple_question_third)
+    
+    open_question_first = models.Question(text='What is the meaning of life', question_type_id=open_type.id)
+    open_question_second = models.Question(text='what is love', question_type_id=open_type.id)
+    session.add(open_question_first)
+    session.add(open_question_second)
+    
+    session.commit()
+
+    questions_list=[multiple_question_first, multiple_question_second, multiple_question_third, open_question_first, open_question_second]
+    random_poll = models.Poll(description='Super Random questions about life', questions=questions_list)
+    session.add(random_poll)
+    session.commit()
+
+    select_statement = select(models.Poll).where(models.Poll.question_type_id == multiple_type.id)
+    response = session.execute(select_statement)
+    queried_poll = response.first()[0]
+    # To Do: 
+    # Having trouble quering for questions by type and poll id
+    # Implement using join
 
 def test_choice_initialized_empty(session):
-    return 
+    pass 
 
 def test_add_choice(session):
-    return 
+    pass 
 
 def test_select_choice(session):
-    return 
+    pass 
 
 def test_remove_choice(session):
-    return 
+    pass 
 
 def test_question_ids_of_poll(session):
-    return
+    pass
 
 # Make it possible to passthrough name as argument    
 def create_users(amount):
     for i in range(amount):
         yield models.User(f'testuser{i}', f'test{i}@email.com')
+
+def questions():
+    while True:
+        text, question_type = yield
+        yield models.Question(text, question_type)
 
 @pytest.fixture
 def session(engine):
@@ -124,10 +218,8 @@ def session(engine):
     Session = sessionmaker(engine)
     session = Session()
     yield session
-    print('going to drop tables')
     session.commit()
     models.Base.metadata.drop_all(engine)
-    print('tables successfully dropped')
 
 @pytest.fixture
 def engine():
